bf706ba42eb3a0573621bf78be5dd3d64f8a8586
Rajangane <pratiksharajangane7@gmail.com>
2021-12-06 12:52:18 +0530

    [Pratiksha] ADD : adding editing displaying and deleting contacts

---
+++ AddresBook_new/Operations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddresBook_new
+{
+    public class Operations
+    {
+        //list declaration to store the personal details
+        List<Contacts> listcontacts = new List<Contacts>();
+
+        public void Add()//add method 
+        {
+            Console.WriteLine("Enter Personal Details : ");
+            Console.Write("First Name : ");
+            string first_name = Console.ReadLine();
+            Console.Write("Last Name : ");
+            string last_name = Console.ReadLine();
+            Console.Write("Address : ");
+            string address = Console.ReadLine();
+            Console.Write("City : ");
+            string city = Console.ReadLine();
+            Console.Write("State : ");
+            string state = Console.ReadLine();
+            Console.Write("Zip Code : ");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Phone No. : ");
+            int phone_no = Convert.ToInt32(Console.ReadLine());
+            Console.Write("E-mail ID : ");
+            string email = Console.ReadLine();
+
+            //adding the details in list
+            listcontacts.Add(new Contacts()
+            {
+                first_name = first_name,
+                last_name = last_name,
+                address = address,
+                city = city,
+                state = state,
+                zip = zip,
+                phone_no = phone_no,
+                email = email
+            });
+        }
+
+        public void Print()//Print method 
+        {
+            foreach (var i in listcontacts)
+            {
+                Console.WriteLine("First Name : " + i.first_name);
+                Console.WriteLine("Last Name : " + i.last_name);
+                Console.WriteLine("Address : " + i.address);
+                Console.WriteLine("City Name : " + i.city);
+                Console.WriteLine("State Name : " + i.state);
+                Console.WriteLine("Zip Code : " + i.zip);
+                Console.WriteLine("Phone No. : " + i.phone_no);
+                Console.WriteLine("Email ID : " + i.email);
+            }
+        }
+        //Edit method 
+        public void Edit(string name, string first, string last, string add, string cityN, string stateN)
+        {
+            int indexOfContact = -1;
+            for (int i = 0; i < listcontacts.Count; i++)
+            {
+                if (listcontacts[i].first_name == name)
+                {
+                    indexOfContact = i;
+                }
+            }
+
+            var editContact = listcontacts[indexOfContact];
+            editContact.first_name = first;
+            editContact.last_name = last;
+            editContact.address = add;
+            editContact.city = cityN;
+            editContact.state = stateN;
+            listcontacts[indexOfContact] = editContact;
+
+        }
+        //delete method
+        public void Delete(string firstname) 
+        {
+            int indexOfContact = -1;
+            for (int i = 0; i < listcontacts.Count; i++)
+            {
+                if (listcontacts[i].first_name == firstname)
+                {
+                    indexOfContact = i;
+                }
+            }
+            listcontacts.RemoveAt(indexOfContact);
+
+        }
+
+
+    }
+}
--- AddresBook_new/Program.cs
+++ AddresBook_new/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("****************** Address Book ****************");
+//Console.WriteLine("****************** Address Book ****************");
+
+ using AddresBook_new;
+
+Console.WriteLine("Address Book Program");
+Operations option = new Operations();  //Creating new object
+int defcount = 0;//counter to break the while loop
+
+while (defcount == 0)
+{
+    Console.WriteLine("1:Add Contact  2:Print Contact  3:Edit Contact  4:Delete  5:Exit");
+    int choice = Convert.ToInt32(Console.ReadLine());//variable for taking choice from the user
+
+    switch (choice)  //switch case
+    {
+        case 1:
+            Console.WriteLine("How many contacts want to add : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                option.Add();
+            }
+            break;
+        case 2:
+            option.Print();//to print contact 
+            break;
+        case 3:
+            Console.WriteLine("Enter name of existing contact which you want to edit :-");
+            Console.WriteLine("Edit Sequence");
+            Console.WriteLine("First name,Last name,Address, City, State");
+            string name = Console.ReadLine();
+            string first = Console.ReadLine();
+            string last = Console.ReadLine();
+            string add = Console.ReadLine();
+            string cityN = Console.ReadLine();
+            string stateN = Console.ReadLine();
+
+            option.Edit(name, first, last, add, cityN, stateN);//to edit contact details
+            break;
+        case 4:
+            Console.WriteLine("Enter First Name To Delete:");
+            string firstname = Console.ReadLine();
+            option.Delete(firstname);//to delete contact
+            break;
+
+        default:
+            Console.WriteLine("End");//default condition
+            defcount++;
+            break;
+    }
+}
+
+        
